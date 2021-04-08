64b110d3086a280b0f4ec3bc3d3be74652bbd578
Joel Hill <joel.hill.87@gmail.com>
2021-04-08 17:36:34 -0600

    Added built in urls for auth

--- whispr/urls.py
+++ whispr/urls.py
@@ -13,12 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from whispr_app.views import StartView
 
+"""
+Adding path('/', include('django.contrib.auth.urls')) provides the following urls:
+
+login/ [name='login']
+logout/ [name='logout']
+password_change/ [name='password_change']
+password_change/done/ [name='password_change_done']
+password_reset/ [name='password_reset']
+password_reset/done/ [name='password_reset_done']
+reset/<uidb64>/<token>/ [name='password_reset_confirm']
+reset/done/ [name='password_reset_complete']
+"""
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', StartView.as_view(), name='start')
 ]
+
+# Appending the static urls for dev environment
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

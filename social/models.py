3fcc2157337b4f17780d674e9a1eb031f50fe218
Joel Hill <joel.hill.87@gmail.com>
2021-04-07 20:25:00 -0600

    Added created and updated fields to post

--- social/models.py
+++ social/models.py
@@ -4,3 +4,5 @@ from profiles.models import Profile
 class Post(models.Model):
     content = models.TextField(max_length=280, blank=False, default='')
     author = models.ForeignKey(to=Profile, related_name='posts', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)

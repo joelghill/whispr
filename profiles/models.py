3181fabbc2f6fd6b39377601b9b3f7e25c5746ab
Joel Hill <joel.hill.87@gmail.com>
2021-04-07 18:40:22 -0600

    Added image field to profile

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(to=User, related_name='profile', on_delete=models.CASCADE)
+    picture = models.ImageField(blank=True)
     bio = models.CharField(max_length=400, blank=True, default='')
     following = models.ManyToManyField(to='profiles.Profile', related_name='followers')
 

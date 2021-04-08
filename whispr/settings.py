3b789409cf51562140a34439a88322e28403e9b0
Joel Hill <joel.hill.87@gmail.com>
2021-04-08 17:36:14 -0600

    Added media root and url to settings

--- whispr/settings.py
+++ whispr/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +118,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# The MEDIA_ROOT is the path on the filesystem to the directory containing your static media.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# The MEDIA_URL is the URL that makes the static media accessible over HTTP.
+MEDIA_URL = '/media/'

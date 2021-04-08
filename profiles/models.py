08d26ca1a919897a7581a9d71780c2b3f13a1894
Joel Hill <joel.hill.87@gmail.com>
2021-04-07 18:34:13 -0600

    Created User Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, related_name='profile', on_delete=models.CASCADE)
+    bio = models.CharField(max_length=400, blank=True, default='')
+    following = models.ManyToManyField(to='profiles.Profile', related_name='followers')
+

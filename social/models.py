0b8b53238c0da60c306002f20ea0367974f65f8f
Joel Hill <joel.hill.87@gmail.com>
2021-04-07 20:01:20 -0600

    Created Post model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from profiles.models import Profile
 
-# Create your models here.
+class Post(models.Model):
+    content = models.TextField(max_length=280, blank=False, default='')
+    author = models.ForeignKey(to=Profile, related_name='posts', on_delete=models.CASCADE)
--- whispr/settings.py
+++ whispr/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles',
-    'whispr_app'
+    'whispr_app',
+    'social'
 ]
 
 MIDDLEWARE = [

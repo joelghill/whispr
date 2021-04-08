9dd9326ac5f5f9d334bea5a0074f770b94d8c7e7
Joel Hill <joel.hill.87@gmail.com>
2021-04-07 20:13:34 -0600

    Added profile to admin

--- profiles/models.py
+++ profiles/models.py
@@ -5,5 +5,8 @@ class Profile(models.Model):
     user = models.OneToOneField(to=User, related_name='profile', on_delete=models.CASCADE)
     picture = models.ImageField(blank=True)
     bio = models.CharField(max_length=400, blank=True, default='')
-    following = models.ManyToManyField(to='profiles.Profile', related_name='followers')
+    following = models.ManyToManyField(to='profiles.Profile', related_name='followers', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
 

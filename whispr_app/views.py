6aed002b25ac77b78454405caa785fce4124d123
Joel Hill <joel.hill.87@gmail.com>
2021-04-12 18:28:30 -0600

    Displaying posts on home page

--- whispr_app/views.py
+++ whispr_app/views.py
@@ -1,3 +1,4 @@
+from social.models import Post
 from typing import ContextManager
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
@@ -62,3 +63,5 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.all()
+        return context
